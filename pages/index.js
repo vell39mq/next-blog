b8f4b8327712bfcd7e24457e1541305d86b0af10
vell39mq <51503089+vell39mq@users.noreply.github.com>
2022-07-12 14:51:32 +0900

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,71 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
 
 export default function Home() {
   return (
-    <div className="container">
+    <Layout home>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>{siteTitle}</title>hbb
       </Head>
-
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
+      <section className={utilStyles.headingMd}>
+        <p>
+          30歳からjavascriptを始めました!
+          <br />
+          IT業界に挑戦したい31歳です（2022/7/12時点）
+          <br />
+          7/8からNext.jsを勉強始めました。このサイトもNext.jsです。
+          <br />
+          Typescriptの勉強もしたいと思っています。
+          <br />
+          未熟者ですが、なにとぞよろしくお願いいたします。
         </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <p>
+          <a href="https://qiita.com/hallotallo">Qiita</a>も始めました（まだ2つしか投稿はありません）。
+        </p>
+        <h2>制作物</h2>
+        <div className={utilStyles.cardContainer}>
+          <section className={utilStyles.card}>
+            <div className={utilStyles.cardContent}>
+              <h1 className={utilStyles.cardTitle}>Chatアプリ</h1>
+              <p className={utilStyles.cardText}>
+                仕様技術:react、firebase（リアルタイムデータベース）、MUI<br/>
+                内容:シンプルなチャットアプリです。reactとfirebaseの練習のために作成しました。
+              </p>
+            </div>
+            <div className={utilStyles.cardLink}>
+              <a href="https://idobata-ham-chat.web.app/">Website</a>
+              <a href="https://github.com/vell39mq/idobata-ham-chat">Github</a>
+            </div>
+          </section>
+          <section className={utilStyles.card}>
+            <div className={utilStyles.cardContent}>
+              <h1 className={utilStyles.cardTitle}>独り言アプリ</h1>
+              <p className={utilStyles.cardText}>
+              仕様技術:react、firebase（auth、firestore）<br/>
+              内容:ユーザー認証と連動するCRUD操作の練習ために作成しました。
+              </p>
+            </div>
+            <div className={utilStyles.cardLink}>
+              <a href="https://nd-blog-project-2ef01.web.app/">Website</a>
+              <a href="https://github.com/vell39mq/react-blog">Github</a>
+            </div>
+          </section>
+          <section className={utilStyles.card}>
+            <div className={utilStyles.cardContent}>
+              <h1 className={utilStyles.cardTitle}>お手製画像縮小.py</h1>
+              <p className={utilStyles.cardText}>
+              仕様技術:python<br/>
+              内容:仕事のパソコンでフリーソフトのインストールが禁止されていたので、自分で画像一括縮小コードを書きました。
+              </p>
+            </div>
+            <div className={utilStyles.cardLink}>
+              <a href="http://webcreatorbox.com/about">Github</a>
+            </div>
+          </section>
         </div>
-      </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
+      </section>
+    </Layout>
+  );
 }
